87b0f3fd752a08fdb3115d6e13ac54b094d3afed
mbecker20 <becker.maxh@gmail.com>
2023-01-29 22:09:54 +0000

    echo empty lines

--- install-starship.sh
+++ install-starship.sh
@@ -6,15 +6,18 @@ mkdir -p $HOME/.starship/bin
 
 curl -sS https://starship.rs/install.sh | sh -s -- --yes --bin-dir $HOME/.starship/bin
 
-if ! grep ".starship/bin" $HOME/.bashrc > /dev/null; then 
+if ! grep ".starship/bin" $HOME/.bashrc > /dev/null; then
+	echo "" >> $HOME/.bashrc
 	echo 'export PATH=$HOME/.starship/bin:$PATH' >> $HOME/.bashrc
 fi
 
-if ! grep "STARSHIP_CONFIG" $HOME/.bashrc > /dev/null; then 
+if ! grep "STARSHIP_CONFIG" $HOME/.bashrc > /dev/null; then
+	echo "" >> $HOME/.bashrc
 	echo 'export STARSHIP_CONFIG=$HOME/.starship/starship.toml' >> $HOME/.bashrc
 fi
 
-if ! grep "starship init bash" $HOME/.bashrc > /dev/null; then 
+if ! grep "starship init bash" $HOME/.bashrc > /dev/null; then
+	echo "" >> $HOME/.bashrc
 	echo 'eval "$(starship init bash)"' >> $HOME/.bashrc
 fi
 
